85f167faf4dd305de2e804523b59ce8c5bc5610d
SauloRdz <saulo.rdz@me.com>
2021-03-04 13:25:51 -0600

    asi es la vida

--- test1.py
+++ test1.py
@@ -1,5 +1,13 @@
-from pylogix import PLC
-with PLC() as comm:
-    comm.IPAddress = '192.168.1.9'
-    ret = comm.Read('MyTagName')
-    print(ret.TagName, ret.Value, ret.Status)
+from pycomm3 import LogixDriver
+
+#with PLC() as comm:
+#    comm.Micro800 = True
+#    comm.IPAddress = '192.168.1.242'
+#    ret = comm.Read('_IO_EM_DI_00')
+#    print(ret.TagName, ret.Value, ret.Status)
+
+def read_single():
+    with LogixDriver('192.168.1.242') as plc:
+        return plc.read('_IO_EM_DI_00')
+
+read_single()
